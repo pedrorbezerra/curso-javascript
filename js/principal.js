8eec1838e55e965681cd62342bf121376ba01df8
Pedro Rodrigues <pedro.rodrigues.bezerra@hotmail.com>
2019-05-09 22:08:57 -0300

    Adicionado Validações
    Adicionei validação do peso e altura, com If e  Else

--- js/principal.js
+++ js/principal.js
@@ -13,8 +13,26 @@ var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
 var imc = peso / (altura * altura);
+var tdImc = paciente.querySelector(".info-imc");
 
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso <= 0 || peso >= 1000) {
+  console.log("Peso inválido");
+  pesoValido = false;
+  tdImc.textContent = "Peso Inválido";
+}
+
+if(altura <= 0 || altura >= 3.00) {
+    console.log("Altura Inválida");
+    alturaValida = false;
+    tdImc.textContent = "Altura Inválido";
+
+}
+
+if(alturaValida && pesoValido) {
 var tdImc= paciente.querySelector(".info-imc");
 tdImc.textContent = imc;
-
+}
 
